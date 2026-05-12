20ad2d8aaf97eaae9a16fe415dc82bd88b934e2c
user <user@1008-021>
2026-05-13 07:53:29 +0800

    加入專案檔案。

---
+++ BookListView/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace BookListView
+{
+    partial class frmBooks
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBooks));
+            this.lvwBooks = new System.Windows.Forms.ListView();
+            this.ImageL = new System.Windows.Forms.ImageList(this.components);
+            this.imageS = new System.Windows.Forms.ImageList(this.components);
+            this.grpView = new System.Windows.Forms.GroupBox();
+            this.cmbView = new System.Windows.Forms.ComboBox();
+            this.grpBorrow = new System.Windows.Forms.GroupBox();
+            this.pnlTools = new System.Windows.Forms.Panel();
+            this.borrowdate = new System.Windows.Forms.DateTimePicker();
+            this.lvwBorrow = new System.Windows.Forms.ListView();
+            this.grpView.SuspendLayout();
+            this.grpBorrow.SuspendLayout();
+            this.pnlTools.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lvwBooks
+            // 
+            this.lvwBooks.Activation = System.Windows.Forms.ItemActivation.TwoClick;
+            this.lvwBooks.HideSelection = false;
+            this.lvwBooks.LargeImageList = this.ImageL;
+            this.lvwBooks.Location = new System.Drawing.Point(3, 3);
+            this.lvwBooks.Name = "lvwBooks";
+            this.lvwBooks.Size = new System.Drawing.Size(478, 404);
+            this.lvwBooks.SmallImageList = this.imageS;
+            this.lvwBooks.TabIndex = 0;
+            this.lvwBooks.UseCompatibleStateImageBehavior = false;
+            this.lvwBooks.ItemActivate += new System.EventHandler(this.lvwBooks_ItemActivate);
+            this.lvwBooks.SelectedIndexChanged += new System.EventHandler(this.lvwBooks_SelectedIndexChanged);
+            // 
+            // ImageL
+            // 
+            this.ImageL.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ImageL.ImageStream")));
+            this.ImageL.TransparentColor = System.Drawing.Color.Transparent;
+            this.ImageL.Images.SetKeyName(0, "Book1.bmp");
+            this.ImageL.Images.SetKeyName(1, "Book2.bmp");
+            this.ImageL.Images.SetKeyName(2, "Book3.bmp");
+            this.ImageL.Images.SetKeyName(3, "Book4.bmp");
+            this.ImageL.Images.SetKeyName(4, "Book5.bmp");
+            this.ImageL.Images.SetKeyName(5, "Book6.bmp");
+            this.ImageL.Images.SetKeyName(6, "Book7.bmp");
+            this.ImageL.Images.SetKeyName(7, "Book8.bmp");
+            // 
+            // imageS
+            // 
+            this.imageS.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageS.ImageStream")));
+            this.imageS.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageS.Images.SetKeyName(0, "Book1.bmp");
+            this.imageS.Images.SetKeyName(1, "Book2.bmp");
+            this.imageS.Images.SetKeyName(2, "Book3.bmp");
+            this.imageS.Images.SetKeyName(3, "Book4.bmp");
+            this.imageS.Images.SetKeyName(4, "Book5.bmp");
+            this.imageS.Images.SetKeyName(5, "Book6.bmp");
+            this.imageS.Images.SetKeyName(6, "Book7.bmp");
+            this.imageS.Images.SetKeyName(7, "Book8.bmp");
+            // 
+            // grpView
+            // 
+            this.grpView.Controls.Add(this.cmbView);
+            this.grpView.Dock = System.Windows.Forms.DockStyle.Top;
+            this.grpView.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.grpView.Location = new System.Drawing.Point(0, 0);
+            this.grpView.Name = "grpView";
+            this.grpView.Size = new System.Drawing.Size(359, 70);
+            this.grpView.TabIndex = 3;
+            this.grpView.TabStop = false;
+            this.grpView.Text = "檢視方式";
+            this.grpView.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // cmbView
+            // 
+            this.cmbView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cmbView.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.cmbView.FormattingEnabled = true;
+            this.cmbView.Location = new System.Drawing.Point(3, 25);
+            this.cmbView.Name = "cmbView";
+            this.cmbView.Size = new System.Drawing.Size(353, 28);
+            this.cmbView.TabIndex = 3;
+            this.cmbView.SelectedIndexChanged += new System.EventHandler(this.cmbView_SelectedIndexChanged);
+            // 
+            // grpBorrow
+            // 
+            this.grpBorrow.Controls.Add(this.lvwBorrow);
+            this.grpBorrow.Controls.Add(this.borrowdate);
+            this.grpBorrow.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpBorrow.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.grpBorrow.Location = new System.Drawing.Point(0, 70);
+            this.grpBorrow.Name = "grpBorrow";
+            this.grpBorrow.Size = new System.Drawing.Size(359, 334);
+            this.grpBorrow.TabIndex = 4;
+            this.grpBorrow.TabStop = false;
+            this.grpBorrow.Text = "借書清單";
+            this.grpBorrow.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // pnlTools
+            // 
+            this.pnlTools.Controls.Add(this.grpBorrow);
+            this.pnlTools.Controls.Add(this.grpView);
+            this.pnlTools.Location = new System.Drawing.Point(487, 3);
+            this.pnlTools.Name = "pnlTools";
+            this.pnlTools.Size = new System.Drawing.Size(359, 404);
+            this.pnlTools.TabIndex = 3;
+            this.pnlTools.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlTools_Paint);
+            // 
+            // borrowdate
+            // 
+            this.borrowdate.Dock = System.Windows.Forms.DockStyle.Top;
+            this.borrowdate.Location = new System.Drawing.Point(3, 25);
+            this.borrowdate.Name = "borrowdate";
+            this.borrowdate.Size = new System.Drawing.Size(353, 29);
+            this.borrowdate.TabIndex = 4;
+            this.borrowdate.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // lvwBorrow
+            // 
+            this.lvwBorrow.Activation = System.Windows.Forms.ItemActivation.TwoClick;
+            this.lvwBorrow.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lvwBorrow.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lvwBorrow.HideSelection = false;
+            this.lvwBorrow.Location = new System.Drawing.Point(3, 60);
+            this.lvwBorrow.Name = "lvwBorrow";
+            this.lvwBorrow.Size = new System.Drawing.Size(353, 271);
+            this.lvwBorrow.TabIndex = 5;
+            this.lvwBorrow.UseCompatibleStateImageBehavior = false;
+            this.lvwBorrow.View = System.Windows.Forms.View.Details;
+            this.lvwBorrow.SelectedIndexChanged += new System.EventHandler(this.lvwBorrow_SelectedIndexChanged);
+            this.lvwBorrow.SelectedIndexChanged += new System.EventHandler(this.lvwBorrow_ItemActivate);
+            // 
+            // frmBooks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1435, 677);
+            this.Controls.Add(this.lvwBooks);
+            this.Controls.Add(this.pnlTools);
+            this.Name = "frmBooks";
+            this.Text = "圖書管理";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpView.ResumeLayout(false);
+            this.grpBorrow.ResumeLayout(false);
+            this.pnlTools.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvwBooks;
+        private System.Windows.Forms.GroupBox grpView;
+        private System.Windows.Forms.ComboBox cmbView;
+        private System.Windows.Forms.GroupBox grpBorrow;
+        private System.Windows.Forms.Panel pnlTools;
+        private System.Windows.Forms.ImageList ImageL;
+        private System.Windows.Forms.ImageList imageS;
+        private System.Windows.Forms.DateTimePicker borrowdate;
+        private System.Windows.Forms.ListView lvwBorrow;
+    }
+}
+
---
+++ BookListView/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookListView
+{
+    public partial class frmBooks : Form
+    {
+
+        string[] b_name = { "三國演義", "西遊記","唐詩三百首","楚辭","西廂記","水滸傳","紅樓夢", "牡丹亭" }; //書名
+        string[] author = {"羅貫中", "吳承恩", "孫洙", "劉向","王實甫","施耐庵", "曹雪芹", "湯顯祖" }; //作者
+        string[] kind = { "章回小說", "章回小說", "詩選", "詩歌", "戲曲","章回小說","章回小說", "戲曲" }; //類別
+        public frmBooks()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbView.Items.Add("大圖示");
+            cmbView.Items.Add("詳細資料");
+            cmbView.Items.Add("小圖示");
+            cmbView.Items.Add("清單");
+            cmbView.Items.Add("大圖示加詳細資料");
+            cmbView.SelectedIndex = 0; //預設選取第一個項目
+            lvwBooks.Columns.Add("書名", 100); //新增 書名 欄位，寬度為100
+            lvwBooks.Columns.Add("作者", 60); //新增 作者 欄位，寬度為60
+            lvwBooks.Columns.Add("類別", 60); //新增 類別 欄位
+            lvwBooks.BeginUpdate(); //暫停重繪
+            for (int i = 0; i < b_name.Length; i++)
+            { //宣告一個ListViewItem物件
+                ListViewItem lvi = new ListViewItem(b_name[i]);
+                lvi.SubItems.Add(author[i].ToString()); //新增 作者 欄位資料
+                lvi.SubItems.Add(kind[i]); //新增 類別 欄位資料
+                lvwBooks.Items.Add(lvi); //新增項目
+                lvwBooks.Items[i].ImageIndex = i; //指定影像的索引值
+            }
+            lvwBooks.EndUpdate(); //重繪;
+
+            lvwBorrow.Columns.Add("書名", 100); //新增 書名 欄位，寬度為100
+            lvwBorrow.Columns.Add("借閱日期", 100);
+            lvwBorrow.Columns.Add("歸還日期", 100);
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlTools_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void lvwBooks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lvwBooks_ItemActivate(object sender, EventArgs e)
+        {
+            if (lvwBooks.SelectedIndices.Count > 0)
+            {
+                string strBookname = b_name[lvwBooks.SelectedIndices[0]];
+                DateTime selectedDate = borrowdate.Value.Date; // 目前選取的日期
+
+                bool isAlreadyBorrowed = false;
+
+                // 遍歷借書清單 lvwBorrow 進行比對
+                foreach (ListViewItem item in lvwBorrow.Items)
+                {
+                    // 1. 先確認書名是否相同
+                    if (item.Text == strBookname)
+                    {
+                        // 2. 取得該書紀錄的到期日 (存放在 SubItems[1])
+                        DateTime dueDate = DateTime.Parse(item.SubItems[1].Text).Date;
+
+                        // 假設借期是 7 天，則起始日是到期日減去 6 天
+                        DateTime startDate = dueDate.AddDays(-6);
+
+                        // 3. 判斷選取日期是否落在 [startDate, dueDate] 區間內
+                        if (selectedDate >= startDate && selectedDate <= dueDate)
+                        {
+                            isAlreadyBorrowed = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (isAlreadyBorrowed)
+                {
+                    MessageBox.Show($"「{strBookname}」在此日期區間 (至 7 天內) 已被借閱，請選擇其他日期或書籍。", "無法借閱");
+                }
+                else
+                {
+                    DialogResult dr = MessageBox.Show($"確定要借閱「{strBookname}」嗎？\n借閱日期：{selectedDate.ToString("yyyy/MM/dd")}", "借書確認", MessageBoxButtons.YesNo);
+                    if (dr == DialogResult.Yes)
+                    {
+                        // 計算到期日 (含當天共 7 天，所以加 6 天)
+                        DateTime startDate = selectedDate.Date;
+                        DateTime dueDate = selectedDate.AddDays(6);
+
+                        lvwBorrow.BeginUpdate();
+                        ListViewItem lvi = new ListViewItem(strBookname);
+                        lvi.SubItems.Add(startDate.ToString("yyyy/MM/dd"));
+                        lvi.SubItems.Add(dueDate.ToString("yyyy/MM/dd"));
+                        lvwBorrow.Items.Add(lvi);
+                        lvwBorrow.EndUpdate();
+                    }
+                }
+            }
+        }
+
+        private void lvwBorrow_ItemActivate(object sender, EventArgs e)
+        {
+            if (lvwBorrow.SelectedIndices.Count > 0)
+            {
+                int index = lvwBorrow.SelectedIndices[0]; 
+                string bookName = lvwBorrow.Items[index].Text; 
+
+                 DialogResult dr = MessageBox.Show($"確定要歸還「{bookName}」嗎?", "還書確認", MessageBoxButtons.YesNo);
+        
+                if (dr == DialogResult.Yes)
+                {
+                    lvwBorrow.Items.RemoveAt(index);
+                }
+                else if(dr == DialogResult.No) {
+                
+                    // 點選「否」的情況
+                    lvwBorrow.SelectedItems[0].Selected = false; // 手動取消選取，讓下次點擊能重新觸發
+                }
+
+                lvwBorrow.Focus(); // 強制讓 ListView 拿回焦點
+                return;
+            }
+        }
+
+        private void cmbView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //根據cmbView的SelectedIndex屬性值，設定檢視方式
+            switch (cmbView.SelectedIndex)
+            {
+                case 0: //大圖示
+                    lvwBooks.View = View.LargeIcon;
+                    break;
+                case 1: //詳細資料
+                    lvwBooks.View = View.Details;
+                    break;
+                case 2: //小圖示
+                    lvwBooks.View = View.SmallIcon;
+                    break;
+                case 3: //清單
+                    lvwBooks.View = View.List;
+                    break;
+                case 4: //大圖示加詳細資料
+                    lvwBooks.View = View.Tile;
+                    break;
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lvwBorrow_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
